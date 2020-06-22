18508eedb99b734e3df08db1ef6c3f82afa08f54
Dereck Goolsby-Bearsong <dgoolsby-bearsong@mii.com>
2020-06-22 10:39:38 -0600

    Completed todoList v2 basic setup, will now have to look at setting it up for Heroku

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require ("mongoose");
+const _ = require('lodash');
 
 const app = express();
 
@@ -11,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 
 // Schema
 const itemsSchema = new mongoose.Schema({
@@ -27,11 +28,12 @@ const Item = mongoose.model("Item", itemsSchema);
 const List = mongoose.model("List", listSchema);
 
 // Variables
-const defaultItems = [{
+const defaultList = [{
   name: "Welcome to your todolist!"}, {
   name: "Hit the + button to add a new item."}, {
   name: "<-- Hit this to delete an item"}];
 
+// Home route
 app.get("/", function(req, res) {
   Item.find({},
    function(err, foundItems){
@@ -51,14 +53,15 @@ app.get("/", function(req, res) {
  });
 });
 
+// Custom Lists Get
 app.get("/:customListName", function(req,res){
-  const customListName = req.params.customListName;
+  const customListName = _.capitalize(req.params.customListName);
   List.findOne({name: customListName}, function(err, foundList){
     if (!err){
       if (!foundList){
         const list = new List({
           name: customListName,
-          items: defaultItems});
+          items: defaultList});
         list.save();
         res.redirect("/" + customListName);
       } else {
@@ -68,33 +71,55 @@ app.get("/:customListName", function(req,res){
   });
 });
 
+// Post for Deleting entries from list
 app.post("/delete", function(req, res){
   const checkedItemId = req.body.checkbox;
-  Item.findByIdAndRemove(checkedItemId, function(err){
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("Successfully removed item from db");
+  const listName = req.body.listName;
+  if (listName === "Today"){
+    Item.findByIdAndRemove(checkedItemId, function(err){
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Successfully removed item from db");
+      }
+    });
+    res.redirect("/");
+  } else {
+    List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemId}}}, function(err, foundList){
+      if (!err){
+        res.redirect("/" + listName);
+      };
     }
-  });
-  res.redirect("/");
+  )};
+
+
 });
 
+// Post for adding entries to list
 app.post("/", function(req, res){
   const itemName = req.body.newItem;
+  const listName = req.body.list;
   const item = new Item({
     name: itemName
   });
-  item.save();
-  res.redirect("/")  ;
+  if (listName === "Today"){
+    item.save();
+    res.redirect("/")  ;
+  } else {
+    List.findOne({name: listName}, function(err, foundList){
+      foundList.items.push(item);
+      foundList.save();
+      res.redirect("/" + listName);
+    });
+  };
 });
 
-
-
+// About Section
 app.get("/about", function(req, res){
   res.render("about");
 });
 
+// Listening function
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
